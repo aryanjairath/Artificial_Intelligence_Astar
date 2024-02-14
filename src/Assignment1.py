c5cc886c9c94a92ee5f06f52affdfc90a3ba8a61
Owner <aryanjairath7@gmail.com>
2024-02-13 20:28:04 -0500

    implemented repeated A*

--- src/Assignment1.py
+++ src/Assignment1.py
@@ -72,25 +72,37 @@ def genMaze(numberOfMazes, rows, cols):
         cmap = colors.ListedColormap(['Red','Green'])
         showMaze(cmap,maze)
         allMazes.append(maze)
-        print(Backward_A_star(maze, starting_coord, dest_coord, rows,cols))
+        print(repeated_A_star(maze, starting_coord, dest_coord, rows,cols))
+        # Adpative_A_star(maze3, starting_coord, dest_coord, rows,cols)
+        np.savetxt('file.txt', maze, delimiter=',')
+
     return allMazes, allManhattan
 
 def A_star(grid, start, end, rows, cols):
-    #visited set of indexes visited
+    expanded = 0
     visited = set()
-    shortest_path = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    f_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    g_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
     direction = [[-1,0], [1,0],[0,-1],[0,1]]
     prev = {(i, j): None for i in range(rows) for j in range(cols)}
     i,j = start[0], start[1]
-    shortest_path[start] = 0
+    f_score[start] = 0
+    g_score[start] = 0
     pq = []  # Initialize the priority queue (heap)
-    heapq.heappush(pq, ((0 + manhattanDistance(start, end)),start))
+    heapq.heappush(pq, ((0 + manhattanDistance(start, end)), start))
 
     while pq:
-        current_distance, current_position = heapq.heappop(pq)
+        file_path = 'file.txt'
+# Open the file for writing ('w' mode) and write lines
+        with open(file_path, 'w') as file:
+            file.write(str(pq))
+
+        _, current_position = heapq.heappop(pq)
+        expanded+=1
         i, j = current_position  # Update i, j to be the current position
 
         if current_position == end:
+            print(expanded)
             return reconstruct_path(grid, prev, end)  # Make sure to return the path
 
         visited.add(current_position)
@@ -102,31 +114,52 @@ def A_star(grid, start, end, rows, cols):
                 continue  # Check grid boundaries and visited or blocked cells
 
             # Calculate the f_score for the neighbor
-            f_distance = shortest_path[(i, j)] + 1 + manhattanDistance((new_i, new_j), end)
-
-            if f_distance < shortest_path[(new_i, new_j)]:
+            f_distance = g_score[(i, j)] + 1 + manhattanDistance((new_i, new_j), end)
+            if g_score[current_position] + 1 < g_score[(new_i, new_j)]:
                 prev[(new_i, new_j)] = current_position  # Update the prev pointer
-                shortest_path[(new_i, new_j)] = f_distance
+                f_score[(new_i, new_j)] = f_distance
+                g_score[(new_i, new_j)] = g_score[current_position] + 1
                 heapq.heappush(pq, (f_distance, (new_i, new_j)))
-    return []  # If the goal is not reached, return an empty path
-    
+    return [] 
+
+def repeated_A_star (grid, start, end, rows, cols):
+    current_start = start
+    imaginary_mat = np.ones((rows,cols))
+    while current_start != end:
+        path = A_star(imaginary_mat, current_start, end, rows, cols)
+        print(path)
+        if not path:
+            break
+        for step in path:
+            if grid[step] == 0:
+                imaginary_mat[step] = 0
+                break
+            else:
+                current_start = step
+        if current_start == end:
+            return path
+    return []
+
 def Backward_A_star(grid, start, end, rows, cols):
-    #visited set of indexes visited
+    expanded = 0
     visited = set()
-    shortest_path = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    f_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    g_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
     direction = [[-1,0], [1,0],[0,-1],[0,1]]
     prev = {(i, j): None for i in range(rows) for j in range(cols)}
     i,j = end[0], end[1]
-    start_i, start_j = start[0], start[1]
-    shortest_path[end] = 0
+    f_score[end] = 0
+    g_score[end] = 0
     pq = []  # Initialize the priority queue (heap)
-    heapq.heappush(pq, ((0 + manhattanDistance((i, start_i), (j, start_j))), end))
+    heapq.heappush(pq, ((0 + manhattanDistance(start, end)), end))
 
     while pq:
-        current_distance, current_position = heapq.heappop(pq)
+        _, current_position = heapq.heappop(pq)
+        expanded+=1
         i, j = current_position  # Update i, j to be the current position
 
         if current_position == start:
+            print(expanded)
             return reconstruct_path(grid, prev, start)  # Make sure to return the path
 
         visited.add(current_position)
@@ -138,24 +171,112 @@ def Backward_A_star(grid, start, end, rows, cols):
                 continue  # Check grid boundaries and visited or blocked cells
 
             # Calculate the f_score for the neighbor
-            f_distance = shortest_path[(i, j)] + 1 + manhattanDistance((new_i, new_j), start)
-
-            if f_distance < shortest_path[(new_i, new_j)]:
-                prev[(new_i, new_j)] = (i, j)  # Update the prev pointer
-                shortest_path[(new_i, new_j)] = f_distance
-                heapq.heappush(pq, (f_distance, (new_i, new_j)))  # Push new position with updated f_score
-
+            f_distance = g_score[(i, j)] + 1 + manhattanDistance((new_i, new_j), start)
+            if g_score[current_position] + 1 < g_score[(new_i, new_j)]:
+                prev[(new_i, new_j)] = current_position  # Update the prev pointer
+                f_score[(new_i, new_j)] = f_distance
+                g_score[(new_i, new_j)] = g_score[current_position] + 1
+                heapq.heappush(pq, (f_distance, (new_i, new_j)))            
     return []  # If the goal is not reached, return an empty path
+
+#Here we prefer larger g_values if the f values are the same
+def A_star_tie(grid, start, end, rows, cols):
+    #visited set of indexes visited
+    file_path = 'file.txt'
+# Open the file for writing ('w' mode) and write lines
+    with open(file_path, 'w') as file:
+        file.write(str(pq))
+    expanded = 0
+    visited = set()
+    f_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    g_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    direction = [[-1,0], [1,0],[0,-1],[0,1]]
+    prev = {(i, j): None for i in range(rows) for j in range(cols)}
+    i,j = start[0], start[1]
+    f_score[start] = 0
+    g_score[start] = 0
+    pq = []  # Initialize the priority queue (heap)
+    heapq.heappush(pq, ((0 + manhattanDistance(start, end)), -1 * g_score[start], start))
+
+    while pq:
+        # print(pq)
+        _, _, current_position = heapq.heappop(pq)
+        i, j = current_position  # Update i, j to be the current position
+        expanded += 1
+        if current_position == end:
+            # print(expanded)
+            return reconstruct_path(grid, prev, end)  # Make sure to return the path
+
+        visited.add(current_position)
+        for d in direction:
+            r, c = d[0], d[1]
+            new_i, new_j = i + r, j + c  # Correctly calculate new_i and new_j
+
+            if not (validRow(new_i) and validCol(new_j)) or (new_i, new_j) in visited or grid[new_i][new_j] == 0:
+                continue  # Check grid boundaries and visited or blocked cells
+
+            # Calculate the f_score for the neighbor
+            f_distance = g_score[(i, j)] + 1 + manhattanDistance((new_i, new_j), end)
+            if g_score[current_position] + 1 < g_score[(new_i, new_j)]:
+                prev[(new_i, new_j)] = current_position  # Update the prev pointer
+                f_score[(new_i, new_j)] = f_distance
+                g_score[(new_i, new_j)] = g_score[current_position] + 1
+                heapq.heappush(pq, (f_distance, -1 * g_score[(new_i, new_j)], (new_i, new_j)))
+    return []  # If the goal is not reached, return an empty path
+
+def Adpative_A_star(grid, start, end, rows, cols):
+    expanded = 0
+    visited = set()
+    f_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    g_score = {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    h_score =  {(i, j): float('inf') for i in range(rows) for j in range(cols)}
+    direction = [[-1,0], [1,0],[0,-1],[0,1]]
+    prev = {(i, j): None for i in range(rows) for j in range(cols)}
+    i,j = start[0], start[1]
+    f_score[start] = 0
+    g_score[start] = 0
+    h_score[start] = manhattanDistance(start, end)
+    pq = []  # Initialize the priority queue (heap)
+    heapq.heappush(pq, ((0 + h_score[start]), start))
+
+    while pq:
+        file_path = 'file.txt'
+# Open the file for writing ('w' mode) and write lines
+        with open(file_path, 'w') as file:
+            file.write(str(pq))
+
+        _, current_position = heapq.heappop(pq)
+        expanded+=1
+        i, j = current_position  # Update i, j to be the current position
+        h_score[i, j] = g_score[end]-g_score[i,j]
+        if current_position == end:
+            print(expanded)
+            return reconstruct_path(grid, prev, end)  # Make sure to return the path
+
+        visited.add(current_position)
+        for d in direction:
+            r, c = d[0], d[1]
+            new_i, new_j = i + r, j + c  # Correctly calculate new_i and new_j
+
+            if not (validRow(new_i) and validCol(new_j)) or (new_i, new_j) in visited or grid[new_i][new_j] == 0:
+                continue  # Check grid boundaries and visited or blocked cells
+
+            # Calculate the f_score for the neighbor
+            f_distance = g_score[(i, j)] + 1 + manhattanDistance((new_i, new_j), end)
+            if g_score[current_position] + 1 < g_score[(new_i, new_j)]:
+                prev[(new_i, new_j)] = current_position  # Update the prev pointer
+                f_score[(new_i, new_j)] = f_distance
+                h_score[i, j] = manhattanDistance((new_i, new_j), end)
+                g_score[(new_i, new_j)] = g_score[current_position] + 1
+                heapq.heappush(pq, (f_distance, (new_i, new_j)))
+    return []
 
 def reconstruct_path(grid, prev, current):
     path = []
     cmap = colors.ListedColormap(['Red','Green','Blue'])
     while current in prev:
-        if current:
-            grid[current[0], current[1]] = 2
         path.append(current)
         current = prev[current]
-    showMaze(cmap, grid)
     path = path[::-1]
     return path
 
@@ -165,7 +286,7 @@ cols = 101
 numMazes = 50
 # mazes = genMaze(numMazes, rows, cols, allMazes)
 
-rows = 6
-cols = 6
+rows = 10
+cols = 10
 numMazes = 1
 mazes2 = genMaze(numMazes, rows, cols)
